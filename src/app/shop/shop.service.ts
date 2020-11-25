6aa5e58fdeb521b08ea0098d589dad0918f4d9e3
danny <dannyfsociety@gmail.com>
2020-11-25 18:58:38 -0400

    Pagination Functionality

--- src/app/shop/shop.service.ts
+++ src/app/shop/shop.service.ts
@@ -4,6 +4,7 @@ import { IBrand } from '../shared/models/brand';
 import { IPagination } from '../shared/models/pagination';
 import { IType } from '../shared/models/productType';
 import { map } from 'rxjs/operators';
+import { ShopParams } from '../shared/models/shopParams';
 
 @Injectable({
   providedIn: 'root'
@@ -13,17 +14,18 @@ export class ShopService {
 
   constructor(private http: HttpClient) { }
 
-  getProducts(brandId?: number, typeId?: number, sort?: string) {
+  getProducts(shopParams: ShopParams) {
     let params = new HttpParams();
 
-    if (brandId) 
-      params = params.append('brandId', brandId.toString());
+    if (shopParams.brandId) 
+      params = params.append('brandId', shopParams.brandId.toString());
     
-    if (typeId)
-      params = params.append('typeId', typeId.toString());
+    if (shopParams.typeId)
+      params = params.append('typeId', shopParams.typeId.toString());
     
-    if (sort) 
-      params = params.append('sort', sort);
+    params = params.append('sort', shopParams.sort);
+    params = params.append('pageIndex', shopParams.pageNumber.toString());
+    params = params.append('pageSize', shopParams.pageSize.toString());
 
     // we use observe 'response' just for using rxjs
     /**
